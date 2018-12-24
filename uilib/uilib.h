7c8a51b37bae34525956548e81d16f8babd25830
suhao <638781@qq.com>
2018-12-24 20:10:26 +0800

    create an empty project. * You should checkout the code from VisualIDE, and update submodule of uilib.*

---
+++ uilib/uilib.h
@@ -0,0 +1,16 @@
+///////////////////////////////////////////////////////////////////////////////////////////
+//
+// Copyright (c) 2017 The Authors of ANT(http:://ant.sh) . All Rights Reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+//
+///////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef UILIB_UILIB_INCLUDE_H_
+#define UILIB_UILIB_INCLUDE_H_
+
+
+
+
+
+#endif  // !UILIB_UILIB_INCLUDE_H_
